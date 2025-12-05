f0dc835abe50083c583b30fbb898f59f554d6147
SimonSuslich <simon.suslich@gmail.com>
2025-12-05 15:31:33 +0100

    Imported tramnetwork.json from lab 1 and implemented class TramNetwork.

--- tram/utils/trams.py
+++ tram/utils/trams.py
@@ -6,9 +6,6 @@ import os
 from .graphs import WeightedGraph
 from django.conf import settings
 
-
-# path changed from Lab 2 version
-# TODO: copy your json file from Lab 1 here
 TRAM_FILE = os.path.join(settings.BASE_DIR, 'static/tramnetwork.json')
 
 
@@ -19,6 +16,61 @@ TRAM_FILE = os.path.join(settings.BASE_DIR, 'static/tramnetwork.json')
         # minlat = min([s._position[0] for s in stops])
         # etc
         # return minlat, minlon, maxlat, maxlon
+
+class TramNetwork(WeightedGraph):
+    def __init__(self, tramdict):
+        super().__init__()
+        self.stopdict = tramdict["stops"]
+        self.linedict = tramdict["lines"]
+        self.timedict = tramdict["times"]
+        for stop in self.stopdict:
+            self.add_vertex(stop)
+        for line, stops in self.linedict.items():
+            i = 0
+            while i < len(stops)-1:
+                self.add_edge(stops[i], stops[i+1])
+                i+=1
+        for stop1, stops in self.timedict.items():
+            for stop2, time in stops.items():
+                self.set_weight(stop1, stop2, time)
+
+    def stop_geo_pos(self, stop):
+        return self.stopdict[stop]        
+
+    def time_between_stops(self, stop1, stop2):
+        return td.time_between_stops(self.linedict, self.timedict, stop1, stop2)
+
+    def distance_between_stops(self, stop1, stop2):
+        return td.distance_between_stops(self.stopdict, stop1, stop2)
+    
+    def lines_via_stop(self, stop):
+        return td.lines_via_stop(self.linedict, stop)
+
+    def lines_stop_list(self, line):
+        return linedict[line]
+    
+    def all_stops_list(self):
+        return list(stop for stop in self.stopdict)
+    
+    def all_lines_list(self):
+        return list(line for line in self.linedict)
+
+    def extreme_positions(self):
+        min_pos = {
+            "lat" : float("inf"),
+            "lon" : float("inf")
+        }
+        max_pos = {
+            "lat" : float("-inf"),
+            "lon" : float("-inf")
+        }
+        for stop, pos in self.stopdict.items():
+            for attr, val in pos.items():
+                if float(val) < min_pos[attr]:
+                    min_pos[attr] = float(val)
+                if float(val) > max_pos[attr]:
+                    max_pos[attr] = float(val)
+        return min_pos["lat"], min_pos["lon"], max_pos["lat"], max_pos["lon"]
 
 
 def readTramNetwork():
