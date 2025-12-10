03b3710163b54feb0cb43f4dc3987c850bf8cf23
SimonSuslich <simon.suslich@gmail.com>
2025-12-10 22:21:00 +0100

    Updated tram/views.py to handle unknown stop names

--- tram/views.py
+++ tram/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .forms import RouteForm
 
 from .utils.tramviz import show_shortest
+from .utils.trams import readTramNetwork
+
+from django.http import HttpResponseBadRequest 
+
 
 def tram_net(request):
     return render(request, 'tram/home.html', {})
@@ -12,7 +16,15 @@ def find_route(request):
         form = RouteForm(request.POST)
         if form.is_valid():
             route = form.data
+            try:
+                G = readTramNetwork()
+                G.stopdict[route["dep"]]
+                G.stopdict[route["dest"]]
+            except KeyError as e:
+                return HttpResponseBadRequest(f"Unknown stop name: {e}")
+            
             timepath, geopath, outfile = show_shortest(route['dep'], route['dest'])
+
             return render(
                 request,
                 'tram/show_route.html',
